670184ee5e94a4a1606d1a60c2926986fc1103ae
Hayden Rode <haydenr76@gmail.com>
2019-03-29 13:14:53 -0400

    Notes on what bare bones needs to be done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ai2project
+
+What we need:
+1. player class to hold and grab data
+2. team class to hold all relavent player objects
+3. neural net
--- nbaMaster.py
+++ nbaMaster.py
@@ -4,3 +4,10 @@ import matplotlib.pyplot as plt
 
 def fuckWithTheData():
 	pass
+
+"""
+What we need:
+1. player class to hold and grab data 
+2. team class to hold all relavent player objects
+3. neural net
+"""
