5fb341202b1c3c4e6c3ba62c0453f9efa5fd5efd
jmanguiat <jmanguiat15@gmail.com>
2019-03-29 14:50:06 -0400

    added team roster data

--- nbaMaster.py
+++ nbaMaster.py
@@ -1,6 +1,18 @@
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-
+import player as p
+import json
 def fuckWithTheData():
 	pass
+
+p1 = p.Player(145)
+data = p1.getSeasonData(2017)
+
+gameNum = 45
+for gameNum in range(1,50):
+	stats = []
+	stats.append(data['data'][gameNum]['player']['last_name'])
+	stats.append(data['data'][gameNum]['pts'])
+	stats.append(data['data'][gameNum]['reb'])
+	stats.append(data['data'][gameNum]['ast'])
+	stats.append(data['data'][gameNum]['blk'])
+	stats.append(data['data'][gameNum]['min'])
+	print(stats)
