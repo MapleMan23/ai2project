456e6d9388cb142d52f990949c1d2296b0088d75
Hayden Rode <haydenr76@gmail.com>
2019-03-29 13:15:27 -0400

    chagnes

--- nbaMaster.py
+++ nbaMaster.py
@@ -4,10 +4,3 @@ import matplotlib.pyplot as plt
 
 def fuckWithTheData():
 	pass
-
-"""
-What we need:
-1. player class to hold and grab data 
-2. team class to hold all relavent player objects
-3. neural net
-"""
