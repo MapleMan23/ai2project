0c596b3c61f80a3101827677fa58d29d8c6bd05c
jmanguiat <jmanguiat15@gmail.com>
2019-04-01 11:10:38 -0400

    fixed our neural net and tried adam and binary_crossentropy for the model opt and loss. Got around 70 pct acc

--- tensorFlow.py
+++ tensorFlow.py
@@ -9,9 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data2008 = np.genfromtxt("train2008.csv", delimiter=',')
-print(data2008.shape)
 data2009 = np.genfromtxt("train2009.csv", delimiter=',')
-print(data2009.shape)
 # data2010 = np.genfromtxt("train2010.csv", delimiter=',')
 # print(data2010.shape)
 
@@ -32,17 +30,17 @@ model = keras.Sequential([
     keras.layers.Flatten(input_shape=(num_features,)),
     keras.layers.Dense(num_features, activation=tf.nn.relu),
     keras.layers.Dense(num_features, activation=tf.nn.relu),
-    keras.layers.Dense(num_features, activation=tf.nn.relu),
-    keras.layers.Dense(2, activation=tf.nn.softmax)
+    keras.layers.Dense(2, activation=tf.nn.softmax),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
 ])
 
 model.compile(
-    optimizer='SGD',
-    loss='mean_squared_error',
+    optimizer='adam',
+    loss='binary_crossentropy',
     metrics=['accuracy']
 )
 
-model.fit(train_data, train_labels, epochs=10)
+model.fit(train_data, train_labels, epochs=20)
 
 test_loss, test_acc = model.evaluate(test_data,test_labels)
 print('Test accuracy: ', test_acc)
